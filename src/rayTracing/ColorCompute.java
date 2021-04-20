eaa36dd3055cd2e509a5bebbe116e26785249885
iristaub <59415717+iristaub@users.noreply.github.com>
2021-04-20 09:43:20 +0300

    Add files via upload

---
+++ src/rayTracing/ColorCompute.java
@@ -0,0 +1,15 @@
+package rayTracing;
+
+import dataTypes.Vec3D;
+import dataTypes.Vec2D;
+import dataTypes.Intersection;
+import dataTypes.Vec;
+
+public class ColorCompute {
+	
+	public static Vec3D getColor(Intersection intersection, Vec3D cameraRay, ) {
+		
+		return new Vec3D(0,0,0,"RGB");
+	}
+
+}
