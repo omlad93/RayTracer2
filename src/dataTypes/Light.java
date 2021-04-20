e70bdfad545cb1350f45a964facc7ff0b9791de0
iristaub <59415717+iristaub@users.noreply.github.com>
2021-04-20 14:30:19 +0300

    Add files via upload

--- src/dataTypes/Light.java
+++ src/dataTypes/Light.java
@@ -29,5 +29,24 @@ public class Light {
 	public Vec3D getNormalizedLightRay(Vec3D point) {
 		return Vec3D.createDistVec(position, point).normalized();
 	}
+	
+	public Vec3D getPosition() {
+		return this.position;
+	}
+	
+	public double getRadius() {
+		return this.radius;
+	}
+	
+	public Vec3D[] findPerpendicularPlane(Vec3D otherLightRay) {
+		double d = this.position.dotProduct(otherLightRay);
+		double x = otherLightRay.getV1();
+		double y = otherLightRay.getV2();
+		double z = otherLightRay.getV3();
+		Vec3D u = new Vec3D(1.0, 0.0 , (double)((-x + d)/ z), "XYZ");
+		Vec3D v = new Vec3D(0.0, 1.0 , (double)((-y + d)/ z), "XYZ");
+		return new Vec3D[] {u.normalized(), v.normalized()};
+		
+	}
 
 }
